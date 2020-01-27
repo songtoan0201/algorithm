9e0b977a9078ce54e8071d6d168c224cbcfacc21
Toan Tran <38516119+songtoan0201@users.noreply.github.com>
2020-01-26 19:26:23 -0800

    changes

---
+++ leetCode/twoSum.py
@@ -0,0 +1,11 @@
+def twoSum(nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for value in nums:
+            print(value)
+        return
+
+print( twoSum([2, 7, 11, 15], 9) )
